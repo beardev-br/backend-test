6ce7a71d400667093d54ef2a3e0b177ffba7077f
Renan Teodoro <renant570@icloud.com>
2023-06-06 10:45:11 -0300

    db migrado para o mySQL

--- Python/projectCRUD/projectCRUD/settings.py
+++ Python/projectCRUD/projectCRUD/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'projectCRUD.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sistema',
+        'PORT': '3306',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
 
