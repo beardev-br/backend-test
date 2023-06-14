acdf53d8e15f06c4d0f9193529e8c9504b3af1e5
Renan Teodoro <renant570@icloud.com>
2023-06-14 08:32:23 -0300

    updating to mySQL

--- Python/projectCRUD/projectCRUD/settings.py
+++ Python/projectCRUD/projectCRUD/settings.py
@@ -78,14 +78,15 @@ WSGI_APPLICATION = 'projectCRUD.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),        
-  #      'ENGINE': 'django.db.backends.mysql',
-  #      'NAME': 'sistema',
-  #      'PORT': '3306',
-  #      'HOST': 'localhost',
-  #      'USER': 'root',
-  #      'PASSWORD': 'Digitaledu$1ZXC',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sistema',
+        'PORT': '3306',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Digitaledu$1ZXC',
+        'OPTIONS': {
+            'init_command': 'SET default_storage_engine=InnoDB',
+        }
     }
 }
 
